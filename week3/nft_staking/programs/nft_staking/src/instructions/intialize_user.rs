928dcd13381ed35a197ae2e27d85581e26c35d69
lalitcap23 <lalitrajput232002@gmail.com>
2025-08-03 05:06:56 +0530

    refined it

--- week3/nft_staking/programs/nft_staking/src/instructions/intialize_user.rs
+++ week3/nft_staking/programs/nft_staking/src/instructions/intialize_user.rs
@@ -10,12 +10,25 @@ pub struct InitializeUser<'info> {
     #[account(
         init,
         payer = user,
-        space = 8 + user_account::INIT_SPACE, // Adjust the space according to your user_account struct
+        space = 8 + user_account::INIT_SPACE, 
         seeds = [b"user_account", user.key().as_ref()],
         bump
 
     )]
 pub user_account:Account<'info, user_account>,
 
+pub system_program: Program<'info, System>
+
    
+}
+impl<'info>IntializeUser<'info>{
+
+    pub fn intialize(&mut self ,bumps:&InitializeUserBumps)-> Result<()>{
+        self.user_account.set.inner(user_account{
+            point :0,
+           amount_staked: 0,
+              bump: bumps.user_account,
+        });
+        Ok(())
+    }
 }
