8349ba080a4584fe2ef09bb0bfaef5d020326bd4
Lalit <lalitrajput232002@gmail.com>
2026-04-25 10:43:30 +0530

    improved the instruction

--- week2/escrow/programs/escrow/src/instructions/make.rs
+++ week2/escrow/programs/escrow/src/instructions/make.rs
@@ -1,33 +1,38 @@
 use anchor_lang::prelude::*;
+
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+    token_interface::{ transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked },
 };
 
-// Define the Escrow account structure
-#[account]
-pub struct Escrow {
-    pub seed: u64,
-    pub maker: Pubkey,
-    pub mint_a: Pubkey,
-    pub mint_b: Pubkey,
-    pub receive: u64,
-    pub bump: u8,
-}
-
-impl Space for Escrow {
-    const INIT_SPACE: usize = 8 + 32 + 32 + 32 + 8 + 1; // seed + maker + mint_a + mint_b + receive + bump
-}
+use crate::state::Escrow;
 
 #[derive(Accounts)]
-#[instruction(seed: u64)]
+#[instruction(seed:u64)]
 pub struct Make<'info> {
+    // maker who creates the account
     #[account(mut)]
     pub maker: Signer<'info>,
-    
+
+    // tokens which sent by the maker
+    #[account(mint::token_program = token_program)]
     pub mint_a: InterfaceAccount<'info, Mint>,
+
+    // token which taker send to maker
+    #[account(mint::token_program = token_program)]
     pub mint_b: InterfaceAccount<'info, Mint>,
-    
+
+    // token account of the maker which will recieve token from taker
+    #[account(
+        init_if_needed,
+        payer = maker,
+        associated_token::mint = mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    // escrow account which holds the state of the escrow
     #[account(
         init,
         payer = maker,
@@ -36,64 +41,52 @@ pub struct Make<'info> {
         space = 8 + Escrow::INIT_SPACE
     )]
     pub escrow: Account<'info, Escrow>,
-    
+
+    // vault account where the maker deposits their tokens
     #[account(
-        init,
+        init_if_needed,
         payer = maker,
         associated_token::mint = mint_a,
         associated_token::authority = escrow,
         associated_token::token_program = token_program
     )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
-    
-    #[account(
-        mut,
-        associated_token::mint = mint_a,
-        associated_token::authority = maker,
-        associated_token::token_program = token_program
-    )]
-    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
-    
+
+    // programs required for the transaction
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 impl<'info> Make<'info> {
-    pub fn init_escrow(
-        &mut self,
-        seed: u64,
-        receive: u64,
-        bumps: &MakeBumps,
-    ) -> Result<()> {
+    // function to initialize the escrow account
+    pub fn init_escrow(&mut self, seed: u64, recieve: u64, bumps: &MakeBumps) -> Result<()> {
+        // set_inner is used to change the all vaules in single instance
         self.escrow.set_inner(Escrow {
             seed,
             maker: self.maker.key(),
             mint_a: self.mint_a.key(),
             mint_b: self.mint_b.key(),
-            receive,
+            recieve,
             bump: bumps.escrow,
         });
         Ok(())
     }
 
-    pub fn deposit(&self, amount: u64) -> Result<()> {
+    // function to deposit the tokens into the escrow vault
+    pub fn deposit(&mut self, amount: u64) -> Result<()> {
+        // it does the verification before the transaction
         let transfer_accounts = TransferChecked {
             from: self.maker_ata_a.to_account_info(),
-            mint: self.mint_a.to_account_info(),
-            to: self.vault.to_account_info(),
             authority: self.maker.to_account_info(),
+            to: self.vault.to_account_info(),
+            mint: self.mint_a.to_account_info(),
         };
 
-        let cpi_ctx = CpiContext::new(
-            self.token_program.to_account_info(),
-            transfer_accounts,
-        );
+        let cpi_ctx = CpiContext::new(self.token_program.to_account_info(), transfer_accounts);
 
-        // Get decimals from the mint account
-        let mint_data = self.mint_a.to_account_info();
-        let mint_account = Mint::try_deserialize(&mut &mint_data.data.borrow()[..])?;
-        
-        transfer_checked(cpi_ctx, amount, mint_account.decimals)
+        // transfer_checked is used to transfer the tokens from maker to vault more securely
+        transfer_checked(cpi_ctx, amount, self.mint_a.decimals)?;
+        Ok(())
     }
 }
